82256ba68fd347017bd152a9c879a7bbb9cb44fb
Trungnguyen <trung14102005@gmail.com>
2026-04-08 22:06:36 +0700

    backend: Tuan 7

---
+++ backend/src/config/mongodb.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        // Lấy địa chỉ kho từ file .env
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('✅ Đã kết nối thành công tới MongoDB!');
+    } catch (error) {
+        console.error('❌ Lỗi kết nối MongoDB:', error.message);
+        // Không cho chạy server nếu database sập
+        process.exit(1); 
+    }
+};
+
+module.exports = connectDB;
